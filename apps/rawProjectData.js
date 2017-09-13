770ec8aa0c76b443c774f79bdae1ddd2446e41ce
Kinley Ramson <kinleyramson@gmail.com>
2017-09-13 10:31:37 -0700

    changed the raw data arrays to object literals

--- apps/rawProjectData.js
+++ apps/rawProjectData.js
@@ -1,10 +1,34 @@
 'use strict';
 
-
-var cfStudy = ['CFStudy','CodeFellows Assignment','../icons/cf.png','Create a website to match a user with study buddies from our pre-existing buddy list, based on the user\'s input','This was our final project for Code 201 at CodeFellows. We were instructed to build a web app with three interlinked HTML pages, use object oriented programming and object constructors, and style our web app using CSS. Together with Ariel, Marco, and Tiger (see the GitHub collaborator page and the README for their info), I made this project in the final week of 201, and presented it on Friday, September 1st 2017.','https://nothingnessbird.github.io/cfstudybuddies/','https://github.com/nothingnessbird/cfstudybuddies','../images/cfStudy.png'];
-
-var busMall = ['Busmall','CodeFellows Assignment','../icons/cf.png','Make a busmall etc','Nice to wheat you! Aw, geez. Connection to Homeworld lost! Connection to Homeworld lost! You don\'t have to try to impress me Morty. Connection to Homeworld lost!Not for the men they cheat on.Nisi fugiat labore veniam quis sit deserunt ut magna nisi. Ullamco incididunt pariatur pariatur deserunt officia enim mollit fugiat consectetur in enim. Exercitation enim est fugiat nostrud aliqua dolore reprehenderit et occaecat sit sunt nulla ut. Ad Lorem eiusmod duis cupidatat mollit exercitation et id nulla fugiat dolore sit cillum est consectetur. Labore amet enim consectetur nulla nostrud irure tempor quis. Magna qui aute nostrud do commodo adipisicing Lorem.','https://nothingnessbird.github.io/busmall/','https://github.com/nothingnessbird/busmall','../images/cfStudy.png'];
-
-var cookies = ['Salmon Cookies','CodeFellows Assignment','../icons/cf.png','cooookies','Nice to wheat you! Aw, geez. Connection to Homeworld lost! Connection to Homeworld lost! You don\'t have to try to impress me Morty. Connection to Homeworld lost!Not for the men they cheat on.Nisi fugiat labore veniam quis sit deserunt ut magna nisi. Ullamco incididunt pariatur pariatur deserunt officia enim mollit fugiat consectetur in enim. Exercitation enim est fugiat nostrud aliqua dolore reprehenderit et occaecat sit sunt nulla ut. Ad Lorem eiusmod duis cupidatat mollit exercitation et id nulla fugiat dolore sit cillum est consectetur. Labore amet enim consectetur nulla nostrud irure tempor quis. Magna qui aute nostrud do commodo adipisicing Lorem.',' https://nothingnessbird.github.io/cookie_stand/','https://github.com/nothingnessbird/cookie_stand','../images/cfStudy.png'];
-
-var projectArray = [cfStudy,busMall,cookies];
+var rawProjectData = [
+  {
+    name:         'CFStudy',
+    type:         'CodeFellows Assignment',
+    icon:         '../icons/cf.png',
+    domain:       'Create a website to match a user with study buddies from our pre-existing buddy list, based on the user\'s input',
+    description:   '<p>This was our final project for Code 201 at CodeFellows.</p> <p>We were instructed to build a web app with three interlinked HTML pages, use object oriented programming and object constructors, and style our web app using CSS.</p> <p>Together with Ariel, Marco, and Tiger (see the GitHub collaborator page and the README for their info), I made this project in the final week of 201, and presented it on Friday, September 1st 2017.</p>',
+    site:         'https://nothingnessbird.github.io/cfstudybuddies/',
+    repo:         'https://github.com/nothingnessbird/cfstudybuddies',
+    pic:            '../images/cfStudy.png'
+  },
+  {
+    name:           'Busmall',
+    type:           'CodeFellows Assignment',
+    icon:           '../icons/cf.png',
+    domain:         'Make a product testing site for potential BusMall products (think SkyMall for buses).',
+    description:    '<p>This is a project from CodeFellows 201, in which we were given product images and instructed to make an app that displayed three at a time for customer to choose between, tracked how many times each was shown and clicked, kept that information in local storage, and displayed the information to the user using graphs.</p><p>Charts.js was used to create the interactive graphs displaying the user\'s behavior data. A random number function was designed to select the images to display.</p>',
+    site:         'https://nothingnessbird.github.io/busmall/',
+    repo:           'https://github.com/nothingnessbird/busmall',
+    pic:            ''
+  },
+  {
+    name:           'Salmon Cookies',
+    type:           'CodeFellows Assignment',
+    icon:           '../icons/cf.png',
+    domain:         'This is a website designed to have an outward facing store front page, and an \'internal\' page to display calculated random cookie sales per hour, per loation, with the ability to add a new store.',
+    description:    '<p>This is a CodeFellows 201 assignment, where we were given a picture and a problem domain, and asked to design and build a site for a fictional cookie store owner, both to display his store info to the public, and to let him estimate average cooke sales per hour at each location.</p><p>There are some fun CSS styles, especially on the internal page on the table of data. There is a random number generator that makes random estimated cookie sales per hour, a function to calculate 12 hour clock displays using 24 hour times, and a totals calculator function.</p>',
+    site:             'https://nothingnessbird.github.io/cookie_stand/',
+    repo:            'https://github.com/nothingnessbird/cookie_stand',
+    pic:            ''
+  }
+]
