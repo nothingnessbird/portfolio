16751d836f900d48eb3aabb5771c63e5300139cc
Kinley Ramson <kinleyramson@gmail.com>
2017-09-14 09:00:29 -0700

    further work on event handling

--- apps/pageView.js
+++ apps/pageView.js
@@ -5,6 +5,10 @@ $('#home-tab').on('click', function(event){
   $('#main-header').css({display: 'block'});
   $('#projects').css({display: 'none'});
   $('#about').css({display: 'none'});
+  $('#plus-sign').css({
+    margin: '10vh',
+    fontSize: '100px'
+  })
   // $('#cog').animateRotate(90, {
   //   duration: 1337,
   //   easing: 'linear',
@@ -18,6 +22,10 @@ $('#about-tab').on('click', function(event){
   $('#main-header').css({display: 'none'});
   $('#projects').css({display: 'none'});
   $('#about').css({display: 'block'});
+  $('#plus-sign').css({
+    margin: '0',
+    fontSize: '50px'
+  })
 })
 
 $('#projects-tab').on('click', function(event){
@@ -25,4 +33,9 @@ $('#projects-tab').on('click', function(event){
   $('#main-header').css({display: 'none'});
   $('#projects').css({display: 'block'});
   $('#about').css({display: 'none'});
+  $('#plus-sign').css({
+    margin: '0',
+    fontSize: '50px',
+    position: 'fixed'
+  })
 })
