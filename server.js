ef703b144bc39a9e05c1cfd7cddb985cba23575c
Kinley Ramson <kinleyramson@gmail.com>
2017-09-19 17:18:53 -0700

    added a get request for the index.html page

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 APP.use(EXPRESS.static('public'));
 
+APP.get('/', function(request, response){
+  response.sendFile('index.html', {root: './public'});
+})
 
 APP.listen(PORT, function() {
   console.log(`Express server currently running on port ${PORT}`);
