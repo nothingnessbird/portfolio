070aedf55d8c2fc62010f5c9fc42fc60085bb187
Kinley Ramson <kinleyramson@gmail.com>
2017-09-26 11:36:57 -0700

    wrapped file in iife, made a method to dimake reduced languages used, display languages used with project render method

--- public/apps/projectRender.js
+++ public/apps/projectRender.js
@@ -1,63 +1,73 @@
 'use strict';
 
-//this file to become and iife
+var app = app || {};
 
-function Project(rawProjObj){
-  this.name = rawProjObj.name;
-  this.type = rawProjObj.type;
-  this.category = rawProjObj.category;
-  this.icon = rawProjObj.icon;
-  this.domain = rawProjObj.domain;
-  this.description = rawProjObj.description;
-  this.site = rawProjObj.site;
-  this.repo = rawProjObj.repo;
-  this.pic = rawProjObj.pic;
-  this.pubDate = rawProjObj.pubDate;
-}
+(function (module) {
+  function Project(rawProjObj){
+    Object.keys(rawProjObj).forEach(key => this[key] = rawProjObj[key]);
+  }
 
-Project.all = [];
+  Project.all = [];
 
-Project.prototype.toHtml = function() {
+  Project.prototype.toHtml = function() {
 
-  let template = Handlebars.compile($('#post-template').html());
+    let template = Handlebars.compile($('#post-template').html());
 
-  this.daysAgo = parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000);
-  this.publishTime = `about ${this.daysAgo} days ago`;
+    this.daysAgo = parseInt((new Date() - new Date(this.pubDate))/60/60/24/1000);
+    this.publishTime = `about ${this.daysAgo} days ago`;
 
-  return template(this);
+    return template(this);
 
-}
+  }
 
-Project.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.pubDate)) - (new Date(a.pubDate));
-  });
+  Project.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.pubDate)) - (new Date(a.pubDate));
+    });
 
-  rawData.forEach(function(projObj) {
-    Project.all.push(new Project(projObj));
-  });
-}
+    Project.all = rawData.map(function(ele){
+      return new Project(ele);
+    })
+  }
 
-function appendProjects() {
-  Project.all.forEach(function(project){
-    $('#projects').append(project.toHtml());
-  });
-}
+  function appendProjects() {
+    Project.all.forEach(function(project){
+      $('#project-display').append(project.toHtml());
+    });
+    Project.languageData().forEach(function(language){
+      $('#languages ul').append($(`<li>${language}</li>`));
+    });
+  }
 
-Project.fetchAll = function() {
-  if (localStorage.rawData) {
+  Project.languageData = function() {
+    return Project.all
+      .reduce(function(aggregator, project){
+        return aggregator.concat(project.languages);
+      }, [])
+      .reduce(function(aggregator, language){
+        if (!aggregator.includes(language)) {
+          aggregator.push(language);
+        }
+        return aggregator;
+      }, []);
+  }
 
-    Project.loadAll(JSON.parse(localStorage.rawData));
-
-    appendProjects();
-
-  } else {
-    $.get('../data/projectJSON.json', function(data){
-      localStorage.rawData = JSON.stringify(data);
+  Project.fetchAll = function() {
+    if (localStorage.rawData) {
 
       Project.loadAll(JSON.parse(localStorage.rawData));
 
       appendProjects();
-    });
+
+    } else {
+      $.get('../data/projectJSON.json', function(data){
+        localStorage.rawData = JSON.stringify(data);
+
+        Project.loadAll(JSON.parse(localStorage.rawData));
+
+        appendProjects();
+      });
+    }
   }
-}
+  module.Project = Project;
+})(app);
