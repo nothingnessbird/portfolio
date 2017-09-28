4251b03704219d47a9d94a4221a4e2da42b1950c
Kinley Ramson <kinleyramson@gmail.com>
2017-09-28 11:51:03 -0700

    added a repo object that requests repos from github, sorts them, and adds modified date attributes

--- public/apps/models/repo.js
+++ public/apps/models/repo.js
@@ -10,13 +10,37 @@ var app = app || {};
 
     $.get('/github/user/repos', function(data){
       repos.all = data;
-      callback();
+      repos.all.sort((a,b) => (new Date(b.updated_at)) - (new Date(a.updated_at)));
+      repos.requestLanaguages(callback);
     })
   };
 
-  // REVIEW: Model method that filters the full collection for repos with a particular attribute.
-  // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
-  repos.with = attr => repos.all.filter(repo => repo[attr]);
+  repos.requestLanaguages = function(callback) {
+    repos.all.forEach(function(repo){
+      if (repo.languages_url) {
+        let url = repo.languages_url;
+        url = url.replace('https://api.github.com','/github');
+        $.get(url, function(data){
+          repo.languagesUsed = Object.keys(data).map(function(language){
+            return `${language}: ${data[language]} characters`;
+          });
+          callback(repo);
+        })
+      }
+    })
+  };
+
+  repos.toHTML = function(repo){
+    let template = Handlebars.compile($('#repo-template').html());
+
+    repo.created = new Date(repo.created_at);
+    repo.created = repo.created.toUTCString();
+    repo.updatedDaysAgo = parseInt((new Date() - new Date(repo.updated_at))/60/60/24/1000);
+    repo.lastUpdateTime = `about ${repo.updatedDaysAgo} days ago`;
+
+    $('#repo-list').append(template(repo));
+
+  }
 
   module.repos = repos;
 })(app);
